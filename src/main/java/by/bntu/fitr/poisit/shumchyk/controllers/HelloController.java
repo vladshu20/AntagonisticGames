84387603f1790b5e2e00bc3e44c0888d9300b1a0
Uladzislau_Shumchyk <vladshu20@gmail,com>
2020-12-02 22:17:20 +0300

    Initial commit

---
+++ src/main/java/by/bntu/fitr/poisit/shumchyk/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package by.bntu.fitr.poisit.shumchyk.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/main/java/by/bntu/fitr/poisit/shumchyk/entities/Problem.java
@@ -0,0 +1,5 @@
+package by.bntu.fitr.poisit.shumchyk.entities;
+
+public class Problem {
+    
+}
