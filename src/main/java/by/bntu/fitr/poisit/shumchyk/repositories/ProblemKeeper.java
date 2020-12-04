4d7a3da7eac2bd0cd37c5e93c5e6bfd1289d04ae
Uladzislau_Shumchyk <vladshu20@gmail,com>
2020-12-04 22:49:44 +0300

    edited form and added data getting from frontend

---
+++ src/main/java/by/bntu/fitr/poisit/shumchyk/repositories/ProblemKeeper.java
@@ -0,0 +1,25 @@
+package by.bntu.fitr.poisit.shumchyk.repositories;
+
+import by.bntu.fitr.poisit.shumchyk.entities.Problem;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProblemKeeper {
+    public List <Problem> problemList = new ArrayList<>();
+
+    public void keepProblem(Problem problem){
+        problemList.add(problem);
+    }
+
+    public Integer[] getPresetData(){
+        Problem pr = problemList.get(0);
+
+        Integer[] ar = {pr.getNumOfRows(),pr.getNumOfCols()};
+
+        return  ar;
+    }
+
+}
